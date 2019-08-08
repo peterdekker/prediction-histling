19d67607113efb920d8835bcbb4b960c308d5514
Peter Dekker <peter.dekker@ivdnt.org>
2019-08-08 14:36:33 +0000

    updated install script

--- install.sh
+++ install.sh
@@ -9,17 +9,19 @@ python3 -m venv env
 # Activate virtual environment
 source env/bin/activate
 # All required packages are instlled via pip in the virtual environment
+#First, install some packages separately, they are needed for building the rest
+pip3 install wheel cython numpy
 pip3 install -r requirements.txt
 # Jupyter Notebook extensions are set up
-jupyter contrib nbextension install --sys-prefix
-jupyter nbextensions_configurator enable --sys-prefix
+#jupyter contrib nbextension install --sys-prefix
+#jupyter nbextensions_configurator enable --sys-prefix
 # Collapsible headings extension is enabled
-jupyter nbextension enable collapsible_headings/main
+#jupyter nbextension enable collapsible_headings/main
 # Remove (possibly) existing Jupyter kernel with name env
 rm -rf ~/.local/share/jupyter/kernels/env
 # Kernel is configured to work with the virtual environment
 python3 -m ipykernel install --user --name env
 
 # Compile documentation
-cd doc
-make html
+#cd doc
+#make html
